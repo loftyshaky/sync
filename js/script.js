d31fd5d2e00b59eddc825eb6b4be5b1ff4b0e16f
loftyshaky <loftyshaky@gmail.com>
2019-12-27 08:58:58 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
 const url = new URL(location.href);
-const locale = url.searchParams.get('hl');
-const success = Boolean(+url.searchParams.get('success'));
+const success_param = url.searchParams.get('success');
+const redirected_by_app = typeof success_param === 'string';
 
-(() => {
-    let el_cls = 'success';
+if (redirected_by_app) {
+    (() => {
+        const success = Boolean(+success_param);
+        let el_cls = 'success';
 
-    if (!success) {
-        el_cls = 'error';
-    }
-
-    x.add_cls(document.body, el_cls);
-    x.remove_cls(s(`.msg.${el_cls}`), 'none');
-})();
-
-(() => {
-    const locale_script = x.create('script', '');
-    locale_script.setAttribute('src', `js/locales/${locale}.js`);
-    x.append(document.body, locale_script);
-
-    locale_script.addEventListener('load', () => {
-        const elements = sa('[data-text]');
-
-        for (const element of elements) {
-            element.textContent = messages[element.dataset.text];
+        if (!success) {
+            el_cls = 'error';
         }
-    });
-})();
+
+        x.add_cls(document.body, el_cls);
+        x.remove_cls(s(`.msg.${el_cls}`), 'none');
+    })();
+
+    (() => {
+        const locale = url.searchParams.get('hl');
+        const locale_script = x.create('script', '');
+
+        locale_script.setAttribute('src', `js/locales/${locale}.js`);
+        x.append(document.body, locale_script);
+
+        locale_script.addEventListener('load', () => {
+            const elements = sa('[data-text]');
+
+            for (const element of elements) {
+                element.textContent = messages[element.dataset.text];
+            }
+        });
+    })();
+}
